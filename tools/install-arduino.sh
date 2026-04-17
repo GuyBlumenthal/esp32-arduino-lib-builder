938020afef7f642e20eb9abed4cd99c5ff764d3a
Guy Blumenthal <guy.blumenthal@outlook.com>
2026-04-17 14:30:57 +0200

    Update git dir

--- tools/install-arduino.sh
+++ tools/install-arduino.sh
@@ -12,7 +12,7 @@ fi
 
 if [ "$AR_BRANCH" ]; then
 	echo "AR_BRANCH='$AR_BRANCH'"
-	git fetch origin '$AR_BRANCH' && \
+	git -C "$AR_COMPS/arduino" fetch origin '$AR_BRANCH' && \
 	git -C "$AR_COMPS/arduino" checkout "origin/$AR_BRANCH"
 fi
 if [ $? -ne 0 ]; then exit 1; fi
